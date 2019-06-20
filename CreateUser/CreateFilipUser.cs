c265a6e78684895f2e852ba443a075b9e8ec97a6
Marie Åhlin <marie.ahlin@consid.se>
2019-06-20 13:14:17 +0200

    Uppdaterat bootstrap fr 3.0.0 till 3.4.1 på uppmaning av GitHub

---
+++ CreateUser/CreateFilipUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using InrappSos.DomainModel;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace CreateUser
+{
+    public class CreateFilipUser
+    {
+        //private ApplicationUserManager _userManager;
+
+
+        //public CreateFilipUser(ApplicationUserManager userManager)
+        //{
+        //    UserManager = userManager;
+        //}
+
+
+        //public ApplicationUserManager UserManager
+        //{
+           
+        //    //get { return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+        //    //private set { _userManager = value; }
+        //}
+
+    }
+}
---
+++ CreateUser/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using InrappSos.DataAccess;
+using InrappSos.DomainModel;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+
+namespace CreateUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("******************  CreateUser startar ***************");
+            //IOwinContext context;
+            //var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(<InrappSosDbContext>()));
+            //var createFilipUser = new CreateFilipUser(manager);
+
+            ////sftpWatcher.CheckFolders();
+
+
+            //Console.WriteLine("********************************************************");
+            //Console.WriteLine("******************  CreateUser klar ***************");
+            ////För test
+            //Console.ReadLine();
+        }
+    }
+}
